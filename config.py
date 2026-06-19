db0d14f692f6b2ce4efbb69b49b06fdac4f871a7
nikhilasaikore <nikhilasaikore@gmail.com>
2026-06-19 15:02:31 +0530

    add to min

--- config.py
+++ config.py
@@ -92,4 +92,4 @@ MONGO_URI = 'mongodb://user:password@host:27017/database'
 # larger or more varied file types if desired.
 CLOUD_STORAGE_BUCKET = 'appengine-storagebucket123'
 MAX_CONTENT_LENGTH = 8 * 1024 * 1024
-ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
